e8e22d82f6e62ee85a46b50b2f055b34c40d686a
collinewait <collinewait17@gmail.com>
2019-06-14 13:44:55 +0300

    add a checkbox field

--- src/App.js
+++ src/App.js
@@ -2,21 +2,26 @@ import React from "react";
 import { withFormik, Form, Field } from "formik";
 import Yup from "yup";
 
-function App() {
+function App({ values }) {
   return (
     <Form>
       <Field type="email" name="email" placeholder="Email" />
       <Field type="password" name="password" placeholder="Password" />
+      <label>
+        <Field type="checkbox" name="newsletter" checked={values.newsletter} />
+        Join our newsletter
+      </label>
       <button>Submit</button>
     </Form>
   );
 }
 
 export default withFormik({
-  mapPropsToValues({ email, password }) {
+  mapPropsToValues({ email, password, newsletter }) {
     return {
       email: email || "",
-      password: password || ""
+      password: password || "",
+      newsletter: newsletter || false
     };
   },
   handleSubmit(values) {

a3091c5d5de5e52555ea04d09a2849aee2e314cd
collinewait <collinewait17@gmail.com>
2019-06-14 12:24:03 +0300

    pass a value to the input

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
-import React from 'react';
-import { withFormik } from 'formik';
-import Yup from 'yup';
+import React from "react";
+import { withFormik } from "formik";
+import Yup from "yup";
 
-function App() {
+function App({ values }) {
   return (
-    <div >
-      <input type="email" name="email" placeHolder="Email" />
+    <div>
+      <input
+        type="email"
+        name="email"
+        placeholder="Email"
+        value={values.email}
+      />
     </div>
   );
 }
 
 export default withFormik({
-
+  mapPropsToValues() {
+    return {
+      email: "test email"
+    };
+  }
 })(App);

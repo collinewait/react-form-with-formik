3dd74e4553d1b5a528e152a92b48147249712a72
collinewait <collinewait17@gmail.com>
2019-06-14 12:03:36 +0300

    create an email input field

--- src/App.js
+++ src/App.js
@@ -1,26 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { withFormik } from 'formik';
+import Yup from 'yup';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      <input type="email" name="email" placeHolder="Email" />
     </div>
   );
 }
 
-export default App;
+export default withFormik({
+
+})(App);

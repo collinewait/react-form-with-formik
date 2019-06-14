e6448cc772bc073c0552d34dc162d08629956226
collinewait <collinewait17@gmail.com>
2019-06-14 14:23:00 +0300

    validate email and password fields

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React from "react";
 import { withFormik, Form, Field } from "formik";
-import Yup from "yup";
+import * as Yup from "yup";
 
-function App({ values }) {
+function App({ values, errors, touched }) {
   return (
     <Form>
-      <Field type="email" name="email" placeholder="Email" />
-      <Field type="password" name="password" placeholder="Password" />
+      <div>
+        {touched.email && errors.email && <p>{errors.email}</p>}
+        <Field type="email" name="email" placeholder="Email" />
+      </div>
+      <div>
+      {touched.password && errors.password && <p>{errors.password}</p>}
+        <Field type="password" name="password" placeholder="Password" />
+      </div>
       <label>
         <Field type="checkbox" name="newsletter" checked={values.newsletter} />
         Join our newsletter
@@ -29,6 +35,14 @@ export default withFormik({
       plan: plan || "free"
     };
   },
+  validationSchema: Yup.object().shape({
+    email: Yup.string()
+      .email()
+      .required(),
+    password: Yup.string()
+      .min(8)
+      .required()
+  }),
   handleSubmit(values) {
     console.log(values);
   }
